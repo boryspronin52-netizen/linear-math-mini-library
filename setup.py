78d71a96cd314f6b28ad4410d71d1c9d94b71e33
Borys Pronin <boryspronin52@gmail.com>
2026-01-08 20:27:12 +0100

    Rename package folder from 'linear math' to 'linear_math'

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="linear-math-mini-library",
     version="0.1.0",
     packages=find_packages(),
-    description="Mini library for linear math",
+    description="Mini library for linear_math",
     author="Borys Pronin",
 )
